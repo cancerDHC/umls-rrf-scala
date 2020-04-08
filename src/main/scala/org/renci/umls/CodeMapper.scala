a60abd7e14e50b12ff6f90686328202dbfa7d545
Gaurav Vaidya <gaurav@renci.org>
2020-04-08 08:47:01 -0400

    Cleaned up output and improved counts.

--- src/main/scala/org/renci/umls/CodeMapper.scala
+++ src/main/scala/org/renci/umls/CodeMapper.scala
@@ -101,16 +101,17 @@ object CodeMapper extends App with LazyLogging {
       val ids = Source.fromFile(conf.idFile()).getLines.map(_.trim).toSeq
       logger.info(s"Filtering to ${ids.size} IDs from ${conf.idFile()}.")
 
+      val halfMapByCode = concepts.getHalfMapsForCodes(conf.fromSource(), ids).groupBy(_.code)
       val map = concepts.getMap(conf.fromSource(), ids, conf.toSource(), Seq.empty)
       val allTermCuis = concepts.getCUIsForCodes(conf.fromSource(), ids)
 
-      stream.println("id\tcount\tterm\tlabels\tparentTerms\tparentLabels")
+      stream.println("fromSource\tid\tlabels\tcountDirect\tcountViaParent\ttoIds\ttoLabels\tncimtIds\tparentSource\tparentIds\tparentLabels")
 
       var count = 0
       val mapByFromId = map.groupBy(_.fromCode)
       val matched = ids.map(id => {
         val maps = mapByFromId.getOrElse(id, Seq())
-        val (parentStr, halfMaps) = if (maps.nonEmpty) ("", Map.empty) else {
+        val (parentStr, halfMaps) = if (maps.nonEmpty) ("", Seq.empty) else {
           val termCuis = allTermCuis.getOrElse(id, Seq.empty)
           // logger.info(s"Checking $termCuis for parent AUI information.")
 
@@ -124,13 +125,13 @@ object CodeMapper extends App with LazyLogging {
           val codes = halfMaps.map(_.code).toSet
           val labels = halfMaps.map(_.label).toSet
 
-          (s"\t$cuis\t$sources\t$codes\t$labels", halfMaps)
+          (s"\t${cuis.mkString("|")}\t${sources.mkString("|")}\t${codes.mkString("|")}\t${labels.mkString("|")}", halfMaps)
         }
 
         stream.println(
-          s"$id\t${maps.size}\t${halfMaps.size}\t"
-            + s"\t${maps.map(m => m.toSource + ":" + m.toCode)}"
-            + s"\t${maps.map(_.labels).mkString("|")}"
+          s"${conf.fromSource()}\t$id\t${halfMapByCode.getOrElse(id, Seq()).map(_.label).mkString("|")}\t${maps.size}\t${halfMaps.size}"
+            + s"\t${maps.map(m => m.toSource + ":" + m.toCode).mkString("|")}"
+            + s"\t${maps.map(_.labels.mkString(";")).mkString("|")}"
             + s"$parentStr"
         )
 
@@ -145,14 +146,14 @@ object CodeMapper extends App with LazyLogging {
 
       val matchedTerm = matched.filter(_._1.nonEmpty).flatMap(_._1)
       val matchedParent = matched.filter(_._2.nonEmpty).flatMap(_._2)
-      val matchedTotal = matchedTerm.size + matchedParent.size
+      val matchedTotal = matched.filter(m => m._1.nonEmpty || m._2.nonEmpty)
 
       val percentageTerm = (matchedTerm.size.toFloat/ids.size) * 100
       val percentageParent = (matchedParent.size.toFloat/ids.size) * 100
-      val percentageTotal = (matchedTotal.toFloat/ids.size) * 100
+      val percentageTotal = (matchedTotal.size.toFloat/ids.size) * 100
       logger.info(f"Matched ${matchedTerm.size} IDs out of ${ids.size} ($percentageTerm%.2f%%)")
       logger.info(f"Matched a further ${matchedParent.size} IDs via the parent term ($percentageParent%.2f%%)")
-      logger.info(f"Total coverage: $matchedTotal IDs out of ${ids.size} ($percentageTotal%.2f%%)")
+      logger.info(f"Total coverage: ${matchedTotal.size} IDs out of ${ids.size} ($percentageTotal%.2f%%)")
     }
 
     stream.close()
