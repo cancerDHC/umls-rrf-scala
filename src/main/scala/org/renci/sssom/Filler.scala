3eedd4fc0e47f38b4d6685022fe87e3db0f7f8ac
Gaurav Vaidya <gaurav@renci.org>
2020-07-31 11:35:58 -0400

    Added a basic interface to the LBO.

--- src/main/scala/org/renci/sssom/Filler.scala
+++ src/main/scala/org/renci/sssom/Filler.scala
@@ -3,6 +3,7 @@ package org.renci.sssom
 import java.io.{File, FileWriter, OutputStreamWriter, PrintWriter}
 
 import com.github.tototoshi.csv.{CSVReader, CSVWriter, TSVFormat}
+import org.renci.sssom.ontologies.LivestockBreedOntologyFiller
 import org.rogach.scallop.{ScallopConf, ScallopOption}
 import org.rogach.scallop.exceptions.ScallopException
 
@@ -32,8 +33,13 @@ object Filler extends App {
     val inputFile: ScallopOption[File] = opt[File](descr = "The SSSOM file to fill in")
     val outputFile: ScallopOption[File] = opt[File](descr = "The output file (in SSSOM format)")
 
-    val fromOntology: ScallopOption[List[File]] = opt[List[File]](
-      descr = "A list of ontologies to search for terms"
+    val fromOntology: ScallopOption[List[String]] = opt[List[String]](
+      descr = "A list of ontologies to search for terms",
+      short = 'o'
+    )
+
+    val fillLivestockBreedOntology: ScallopOption[Boolean] = opt[Boolean](
+      descr = "Fill terms from the Livestock Breed Ontology"
     )
 
     val fillPredicateId: ScallopOption[String] = opt[String](
@@ -48,6 +54,18 @@ object Filler extends App {
   val inputSource = if(conf.inputFile.isSupplied) Source.fromFile(conf.inputFile()) else Source.fromInputStream(System.in, "UTF-8")
   val outputWriter = if(conf.outputFile.isSupplied) new PrintWriter(new FileWriter(conf.outputFile())) else new OutputStreamWriter(System.out)
 
+  // Build a list of SSSOMRowFillers that we need to apply here.
+  val rowFillers: Seq[SSSOMFiller] = (
+    (if (conf.fillLivestockBreedOntology()) Some(new LivestockBreedOntologyFiller()) else None)
+    :: Nil
+    ).flatten
+  scribe.info(s"Active row fillers: ${rowFillers.mkString(", ")}")
+
+  if (rowFillers.isEmpty) {
+    scribe.error("No row fillers set! Use --help to see a list of possible filters.")
+    System.exit(1)
+  }
+
   // Read input as TSV.
   val reader = CSVReader.open(inputSource)(new TSVFormat {})
   val (headers, rows) = reader.allWithOrderedHeaders()
@@ -56,9 +74,20 @@ object Filler extends App {
   writer.writeRow(headers)
   rows.map(row => {
     // Should we fill in this row?
-    if(conf.fillPredicateId.isSupplied && row.getOrElse("predicate_id", "") == conf.fillPredicateId()) {
-      // Apply all the SSSOMRowFillers that are relevant here.
-      row
+    val subjectId = row.getOrElse("subject_id", "(none)")
+    val predicateId = row.getOrElse("predicate_id", "")
+    if(predicateId.isEmpty || (!conf.fillPredicateId.isSupplied || predicateId == conf.fillPredicateId())) {
+      scribe.info(s"Looking for a match for ${row.getOrElse("subject_id", "")} (${row.getOrElse("subject_label", "")})")
+      val optMatchResult = rowFillers.to(LazyList).flatMap(_.fillRow(row, headers)).headOption
+      if (optMatchResult.isEmpty) {
+        scribe.info(s"Could not fill subject ID $subjectId: no fillers matched")
+        row
+      } else {
+        val result = optMatchResult.head
+
+        scribe.info(s"Filled subject ID $subjectId with ${result.filler}")
+        result.result
+      }
     } else row
   }).foreach(row => {
     // Write out each row in the correct order.
---
+++ src/main/scala/org/renci/sssom/ontologies/LivestockBreedOntologyFiller.scala
@@ -0,0 +1,28 @@
+package org.renci.sssom.ontologies
+
+import org.renci.sssom.SSSOMFiller
+import org.renci.sssom.SSSOMFiller.Row
+
+/**
+  * Looks up terms in the Livestock Breed Ontology (http://bioportal.bioontology.org/ontologies/LBO).
+  * We need a custom handler for this because a lot of our terms are in the format "Jamaica Red cattle breed"
+  * while LBO has it in the format "Jamaica Red" (subclass of "cattle breed", http://purl.obolibrary.org/obo/LBO_0000144).
+  */
+class LivestockBreedOntologyFiller extends SSSOMFiller {
+  override def toString: String = "LivestockBreedOntologyFiller()"
+
+  /**
+    * Fill in the input row. The list of all headers is also provided.
+    *
+    * @return None if this row could not be filled, and Some[Row] if it can.
+    */
+  override def fillRow(row: Row, headers: List[String]): Option[SSSOMFiller.Result] = {
+    val subjectId = row.getOrElse("subject_id", "(none)")
+    val subjectLabels = row.getOrElse("subject_label", "").split("\\s*\\|\\s*").toSet
+
+    val regex = "^(.*) cattle breed$".r
+    subjectLabels.to(LazyList).flatMap({
+      case regex(breedName) => Some(SSSOMFiller.Result(row, row, this))
+    }).headOption
+  }
+}
